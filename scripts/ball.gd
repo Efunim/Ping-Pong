de0ede0e9c9881adc1c6f6cd08578206238975cc
Efunim <eugenia.majako@gmail.com>
2026-02-07 22:34:25 +0300

    feat: added score zones

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,17 +3,18 @@ extends CharacterBody2D
 @export var speed: float = 200
 @export var max_speed: float = 400
 
-var direction_y: int = -1 # Shows which playe's side it is
-
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity * speed * delta, false, 0.1)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
 		
-	if speed < max_speed:
-		speed += 0.5
+		var reflection = collision.get_remainder().bounce(collision.get_normal())
+		move_and_collide(reflection)
+		
+		if speed < max_speed:
+			speed += 10
 
-func launch():
+func launch(direction_y: int = -1): # Shows which playe's side it is, -1 means player
 	# Randomise ball angle between around 215 and 330 deg
 	# We need to change angle only on X axis, so I used Vector2.RIGHT
 	velocity = Vector2.RIGHT.rotated(randf_range(3.5, 5.5))
